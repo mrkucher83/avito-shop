2760589fa4ecb2aa38f6b0ba06bc03e2145901fc
alexanderkucher <mr.kucher83@mail.ru>
2025-02-10 18:57:32 +0400

    init logger and router chi

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/mrkucher83/avito-shop/internal/routes"
+	"github.com/mrkucher83/avito-shop/pkg/logger"
+	"syscall"
+)
+
+const DefaultPort = "8080"
+
 func main() {
-	//
+	logger.InitLogger(logger.NewLogrusLogger())
+
+	port := DefaultPort
+	if value, ok := syscall.Getenv("AVITO_SHOP_PORT"); ok {
+		port = value
+	}
+
+	routes.Start(port)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/mrkucher83/avito-shop
 
 go 1.22.3
+
+require (
+	github.com/fabienm/go-logrus-formatters v1.0.0 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
